e109baa1d006b6d11c8fcd3ba6b5a0535f1971e3
Kasper K <kakr@iba.dk>
2022-11-21 10:23:00 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+//build a house with a door, a window, and a roof
+
+System.Console.WriteLine("   *  ");
+System.Console.WriteLine("  /*\\ ");
+System.Console.WriteLine(" /***\\ ");
+System.Console.WriteLine(" -----");
+System.Console.WriteLine("|     |");
+System.Console.WriteLine("|     |");
+System.Console.WriteLine("|_____|");
